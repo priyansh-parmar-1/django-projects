86d53fea89c897d6777311323ae3bbb834382596
MAHIMA\mahim <mahimasuhagiya@gmail.com>
2024-04-07 13:41:13 +0530

    changes in invoice

--- CarRental/customer_portal/views.py
+++ CarRental/customer_portal/views.py
@@ -178,7 +178,8 @@ def carDetails(request, car_id):
 def downloadinvoice(request, booking_id):
     booking = Booking.objects.get(booking_id=booking_id)
     template = get_template('invoice.html')
-    html = template.render({'booking': booking})
+    curdate = datetime.now()
+    html = template.render({'booking': booking,'date': curdate})
     # Create PDF
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="invoice.pdf"'
